4234ef18850478ea9979abd3a29b431dfcb52b78
KN <knejman56@gmail.com>
2020-05-02 23:49:01 +0200

    dice with state

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +16,54 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void randomNumberLeft()
+  {
+    setState(() {
+      leftDiceNumber = newRandomNumber();
+    });
+  }
+
+  void randomNumberRight()
+  {
+    setState(() {
+      rightDiceNumber = newRandomNumber();
+    });
+  }
+
+  int newRandomNumber()
+  {
+    return Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: randomNumberLeft,
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: randomNumberRight,
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
